cac94fb34604a2f269c203e809368e9a4c02fcef
PavloDymohlo <pavelanatolevic777@gmail.com>
2024-02-25 00:01:01 +0200

    add all files

---
+++ SpringSecurityDemo/src/main/java/controller/UserController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+    @GetMapping("/user")
+    public ResponseEntity<String> user() {
+        return ResponseEntity.ok("Hello user!");
+    }
+    @GetMapping("/userVip")
+    @PreAuthorize("hasAuthority('VIP')")
+    public ResponseEntity<String> userVip() {
+        return ResponseEntity.ok("Hello VIP!");
+    }
+
+    @GetMapping("/userPremium")
+    @PreAuthorize("hasAuthority('PREMIUM')")
+    public ResponseEntity<String> userPremium() {
+        return ResponseEntity.ok("Hello Premium!");
+    }
+
+    @GetMapping("/userAdmin")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<String> userAdmin() {
+        return ResponseEntity.ok("Hello Admin!");
+    }
+}
